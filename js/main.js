1027dd28b166f13f8cbf32f083c13571d725ef7b
ahmadhazm <1ahmadhazm@gmail.com>
2021-10-24 10:43:01 +0300

    more info page js

--- js/main.js
+++ js/main.js
@@ -6,30 +6,6 @@ searchBtn.addEventListener("click",()=>{
     search()
 })
 
-// relod the page when click on header
-function relodPage(){
-    let cards = document.querySelectorAll(".card")
-    cards.forEach(e=>{
-        e.remove()
-    })
-
-    Featch("")
-}
-
-// search
-function search(){
-    let searchValue = searchBar.value
-    let regx = /\s/ig
-    searchValue = searchValue.replace(regx, "%20")
-
-    let cards = document.querySelectorAll(".card")
-    cards.forEach(e=>{
-        e.remove()
-    })
-
-    Featch("?title=" + searchValue)
-}
-
 // show cards when page loded
 addEventListener("load",Featch(""))
 function Featch(requist){
@@ -47,53 +23,153 @@ function Featch(requist){
     })
 }
 
+// search
+function search(){
+    let searchValue = searchBar.value
+    let regx = /\s/ig
+    searchValue = searchValue.replace(regx, "%20")
+
+    let cards = document.querySelectorAll(".card")
+    cards.forEach(e=>{
+        e.remove()
+    })
+
+    Featch("?title=" + searchValue)
+}
+
+// relod the page when click on header
+function relodPage(){
+    let cards = document.querySelectorAll(".card")
+    cards.forEach(e=>{
+        e.remove()
+    })
+
+    Featch("")
+}
+
 // card class
 class card{
     constructor(data, title_max_length){
         this.data = data
-        this.title_max_length = title_max_length;
+        this.title_max_length= title_max_length;
     }
     
     createCard(){
         // card title
-        let card_title = this.data.titles.en;
-        if(card_title.length > this.title_max_length){
-            card_title = card_title.substr(0, this.title_max_length)
-            card_title = card_title + "..."
+        this.card_title = this.data.titles.en;
+        
+        function clearTitel(title, max_length){
+            if(title.length > max_length){
+                return title.substr(0, max_length) + "..."
+            }
+            return title
         }
 
         // years
-        let start_date = this.data.start_date.substr(0, 10) ;        
-        let end_date = this.data.end_date.substr(0, 10);
+        this.start_date = this.data.start_date.substr(0, 10);
+        this.end_date = this.data.end_date.substr(0, 10);
 
-        let card_img_url = this.data.cover_image;
-        let episodes_count= this.data.episodes_count;
-        let score = this.data.score
+        this.banner_image = this.data.banner_image;
+        this.card_img_url = this.data.cover_image;
+        this.episodes_count= this.data.episodes_count;
+        this.score =     this.data.score
 
         // card html
         let card_html = `
             <div class="img-container">
-                <img class="card-img" src="${card_img_url}" alt="${card_title}">
+                <img class="card-img" src="${this.card_img_url}" alt="${this.card_title}">
             </div>
             <div class="card-info">
                 <ul class="info-1">
-                    <li class="title">${card_title}</li>
-                    <li class="episotes-num">ep: ${episodes_count}</li>
+                    <li class="title">${clearTitel(this.card_title, this.title_max_length)}</li>
+                    <li class="episotes-num">ep: ${this.episodes_count}</li>
                 </ul>   
                 <ul class="info-2">
-                    <li class="card-info-text">Start: ${start_date}</li>
-                    <li class="card-info-text">End: ${end_date}</li>
+                    <li class="card-info-text">Start: ${this.start_date}</li>
+                    <li class="card-info-text">End: ${this.end_date}</li>
                 </ul>
             </div>`
 
         // creating and appending the card
         let cards_container = document.querySelector(".cards-container")
         let card = document.createElement("li")
+        card.addEventListener("click", this.createMoreInfo)
 
         card.classList.add("card")
         card.innerHTML = card_html;
 
         cards_container.append(card)
+        
+        return card;
+    }
+    
+    showMoreInfo(){
+        this.jp_title = this.data.titles.jp
+        this.episode_duration = this.data.episode_duration
+
+        let more_info_page_code = `
+        <button class="get_home_btn">Home</button>
+        
+        <img class="card-img" src="${this.banner_image}" alt="${this.card_title}">
+        <h2>
+            ${this.card_title}
+        </h2>
+
+        <div class="info">
+            <ul class="info-1">
+                <li class="card-info-text">
+                    JP Name: ${this.jp_title}
+                </li>
+                
+                <li class="card-info-text">
+                    Start: ${this.start_date}
+                </li>
+
+                <li class="card-info-text">
+                    End: ${this.end_date}
+                </li>
+            </ul>
+
+            <ul class="info-2">
+                <li class="card-info-text">
+                    Episodes: ${this.episodes_count}
+                </li>
+
+                <li class="card-info-text">
+                    Episode Duration: ${this.episode_duration}
+                </li>
+
+                <li class="card-info-text">
+                    Score: ${this.score}
+                </li>
+            </ul>
+        </div>        
+        `
+        console.log(this.data.trailer_url)
+        if(this.data.trailer_url != undefined)
+            more_info_page_code += `<iframe class="iframe" src="${this.data.trailer_url}" frameborder="0" allowfullscreen></iframe>`
+        console.log(more_info_page_code)
+        
+
+        const main = document.querySelector("main")
+        const more_info_page = document.createElement("section")
+
+        const cards_container = document.querySelector(".cards-container")
+        const header = document.querySelector("header")
+        cards_container.style.display = "none"
+        header.style.display = "none"
+
+        more_info_page.innerHTML = more_info_page_code;
+        more_info_page.classList.add("more_info_page")
+
+        main.append(more_info_page)
+
+        let get_home_btn = document.querySelector(".get_home_btn")
+        get_home_btn.addEventListener("click",()=>{
+            more_info_page.remove()
+            cards_container.style.display = ""
+            header.style.display = ""
+        })
     }
 }
 
@@ -101,6 +177,8 @@ class card{
 function createCard(data){
     data.forEach(cardData => {
         let c = new card(cardData, 20)
-        c.createCard()
+        c.createCard().addEventListener("click",()=>{
+            c.showMoreInfo()
+        })        
     });
 }
